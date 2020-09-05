a103b1ffd524a9223d2347ccf12bb6f0417c7394
Janitha Tennakoon <janitha000@gmail.com>
2020-09-05 21:52:05 +0800

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h2>Welcome to My-Site</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <h3>Janitha Tennakoon</h3>
+
+            </div>
+            <div className='header_right'>
+                <h3>About</h3>
+                <h3>Contact Me</h3>
+            </div>
+            This is Header
+        </div>
+    )
+}
+
+export default Header;
